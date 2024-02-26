e736379b4a736009ad966e374d863b66f2f8e003
FunghiApe <funghiape@gmail.com>
2024-02-26 09:57:07 +0800

    Initial Commit

---
+++ LengthConvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LengthConvert
+//
+//  Created by FunghiApe on 2024/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LengthConvert/LengthConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConvertApp.swift
+//  LengthConvert
+//
+//  Created by FunghiApe on 2024/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LengthConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
